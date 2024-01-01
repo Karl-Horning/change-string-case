4b3170f3a7394cf236d1dcda3d7bed56169643e3
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-01-01 09:51:03 +0000

    Original files copied

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+// jshint esversion: 6
+
+const pressed = [];
+const secretCode = 'wooooooooooo';
+
+window.addEventListener('keyup', (e) => {
+  console.log(e);
+  pressed.push(e.key);
+  pressed.splice(- secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join('').includes(secretCode)) {
+    console.log('Ding Ding!');
+    cornify_add();
+  }
+  console.log(pressed);
+});
+
+function formatString() {
+  let inputOutput = document.getElementById('string');
+  let str = String(inputOutput.value);
+
+  // outputs string by removing punctuation and replacing spaces with hyphens
+  str = str.replace(/[.,\/#!?$%\^&\*;:{}=\_"'`~()]/g, "");
+  str = str.replace(/\s+/g, '-');
+  str = str.toLowerCase();
+  inputOutput.value = str;
+
+  // copy formatted string to clipboard
+  inputOutput.select();
+  document.execCommand("Copy");
+}
