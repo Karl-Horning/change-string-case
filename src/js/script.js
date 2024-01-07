ca25ad0c3209c81d545c7e342b23f1bcfdcb7ced
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-01-07 22:24:22 +0000

    Added comments, focus on text input on page load and refactored code

--- src/js/script.js
+++ src/js/script.js
@@ -1,177 +1,202 @@
-const pressed = [];
-const secretCode = "wooooooooooo";
-const searchButton = document.getElementById("search-button");
-const stringToFormat = document.getElementById("string-to-format");
-
-/**
- * Handles key events for secret code checking.
- *
- * @param {KeyboardEvent} e - The key event.
+/*
+ * This script handles input string formatting and includes a secret code check.
  */
-const handleSecretCodeCheck = (e) => {
-    pressed.push(e.key);
-    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-    if (pressed.join("").includes(secretCode)) {
-        console.log("Ding Ding!");
-        cornify_add();
-    }
-};
+(() => {
+    const pressed = [];
+    const secretCode = "wooooooooooo";
+    const searchButton = document.getElementById("search-button");
+    const stringToFormatInput = document.getElementById("string-to-format");
 
-/**
- * Converts a string to snake_case.
- *
- * @param {string} str - The input string.
- * @returns {string} - The formatted string.
- * Example of using toSnakeCase:
- * @example
- * const inputString = 'This is  a   _sample-string  with--special___characters! and hyphens_';
- * const snakeCaseResult = toSnakeCase(inputString);
- * console.log(snakeCaseResult);
- * // Output: 'this_is_a_sample_string_with_special_characters_and_hyphens'
- */
-const toSnakeCase = (str) => {
-    return (
-        str
-            // Add spaces before capital letters
-            .replace(/([A-Z])/g, " $1")
+    /**
+     * Handles key events for secret code checking.
+     *
+     * @param {KeyboardEvent} e - The key event.
+     */
+    const handleSecretCodeCheck = (e) => {
+        pressed.push(e.key);
+        pressed.splice(
+            -secretCode.length - 1,
+            pressed.length - secretCode.length
+        );
+        if (pressed.join("").includes(secretCode)) {
+            console.log("Ding Ding!");
+            cornify_add();
+        }
+    };
 
-            // Replace multiple hyphens with a single hyphen
-            .replace(/-+/g, "-")
+    /**
+     * Converts a string to snake_case.
+     *
+     * @param {string} str - The input string.
+     * @returns {string} - The formatted string.
+     * Example of using toSnakeCase:
+     * @example
+     * const inputString = 'This is  a   _sample-string  with--special___characters! and hyphens_';
+     * const snakeCaseResult = toSnakeCase(inputString);
+     * console.log(snakeCaseResult);
+     * // Output: 'this_is_a_sample_string_with_special_characters_and_hyphens'
+     */
+    const toSnakeCase = (str) => {
+        return (
+            str
+                // Add spaces before capital letters
+                .replace(/([A-Z])/g, " $1")
 
-            // Replace whitespace characters and hyphens with underscores
-            .replace(/[\s-]+/g, "_")
+                // Replace multiple hyphens with a single hyphen
+                .replace(/-+/g, "-")
 
-            // Remove all special characters except underscores
-            .replace(/[^\w\s]|_/g, "_")
+                // Replace whitespace characters and hyphens with underscores
+                .replace(/[\s-]+/g, "_")
 
-            // Replace multiple underscores with a single underscore
-            .replace(/_+/g, "_")
+                // Remove all special characters except underscores
+                .replace(/[^\w\s]|_/g, "_")
 
-            // Remove special characters from the beginning and end of the string
-            .replace(/^[^a-zA-Z0-9]+/, "")
-            .replace(/[^a-zA-Z0-9]+$/, "")
+                // Replace multiple underscores with a single underscore
+                .replace(/_+/g, "_")
 
-            // Convert the string to lowercase
-            .toLowerCase()
-    );
-};
+                // Remove special characters from the beginning and end of the string
+                .replace(/^[^a-zA-Z0-9]+/, "")
+                .replace(/[^a-zA-Z0-9]+$/, "")
 
-/**
- * Converts a string to kebab-case.
- *
- * @param {string} str - The input string.
- * @returns {string} - The formatted string.
- */
-const toKebabCase = (str) => {
-    return (
-        str
-            // Add spaces before capital letters
-            .replace(/([A-Z])/g, " $1")
+                // Convert the string to lowercase
+                .toLowerCase()
+        );
+    };
 
-            // Replace multiple hyphens with a single hyphen
-            .replace(/-+/g, "-")
+    /**
+     * Converts a string to kebab-case.
+     *
+     * @param {string} str - The input string.
+     * @returns {string} - The formatted string.
+     */
+    const toKebabCase = (str) => {
+        return (
+            str
+                // Add spaces before capital letters
+                .replace(/([A-Z])/g, " $1")
 
-            // Replace whitespace characters and underscores with hyphens
-            .replace(/[\s_]+/g, "-")
+                // Replace multiple hyphens with a single hyphen
+                .replace(/-+/g, "-")
 
-            // Remove all special characters except hyphens
-            .replace(/[^\w\s-]|_/g, "")
+                // Replace whitespace characters and underscores with hyphens
+                .replace(/[\s_]+/g, "-")
 
-            // Replace multiple hyphens with a single hyphen
-            .replace(/-+/g, "-")
+                // Remove all special characters except hyphens
+                .replace(/[^\w\s-]|_/g, "")
 
-            // Remove hyphens from the beginning and end of the string
-            .replace(/^-+/, "")
-            .replace(/-+$/, "")
+                // Replace multiple hyphens with a single hyphen
+                .replace(/-+/g, "-")
 
-            // Convert the string to lowercase
-            .toLowerCase()
-    );
-};
+                // Remove hyphens from the beginning and end of the string
+                .replace(/^-+/, "")
+                .replace(/-+$/, "")
 
-/**
- * Converts a string to PascalCase.
- *
- * @param {string} str - The input string.
- * @returns {string} - The formatted string.
- */
-const toPascalCase = (str) => {
-    return (
-        str
-            // Replace all whitespace, special characters, and underscores with a single space
-            .replace(/[^\w\s]|_+/g, " ")
-            .replace(/\s+/g, " ")
-            // Remove whitespace from beginning and end
-            .trim()
-            // Capitalise the first letter of each word
-            .replace(/(?:^|\W|_)\w/g, (match) => {
-                return match.toUpperCase();
-            })
-            // Replace whitespace characters
-            .replace(/[\s]+/g, "")
-    );
-};
+                // Convert the string to lowercase
+                .toLowerCase()
+        );
+    };
 
-/**
- * Converts a string to camelCase.
- *
- * @param {string} str - The input string.
- * @returns {string} - The formatted string.
- */
-const toCamelCase = (str) => {
-    // Use the toPascalCase function to format the string
-    const pascalCase = toPascalCase(str);
-    // Make the first character in a string lower case
-    return pascalCase.charAt(0).toLowerCase() + pascalCase.slice(1);
-};
+    /**
+     * Converts a string to PascalCase.
+     *
+     * @param {string} str - The input string.
+     * @returns {string} - The formatted string.
+     */
+    const toPascalCase = (str) => {
+        return (
+            str
+                // Replace all whitespace, special characters, and underscores with a single space
+                .replace(/[^\w\s]|_+/g, " ")
+                .replace(/\s+/g, " ")
+                // Remove whitespace from beginning and end
+                .trim()
+                // Capitalise the first letter of each word
+                .replace(/(?:^|\W|_)\w/g, (match) => {
+                    return match.toUpperCase();
+                })
+                // Replace whitespace characters
+                .replace(/[\s]+/g, "")
+        );
+    };
 
-/**
- * Formats the input string based on the selected case and copies it to the clipboard.
- */
-const formatString = () => {
-    const stringToFormat = document.getElementById("string-to-format");
-    const caseSelector = document.getElementById("case-select");
-    let formattedString = String(stringToFormat.value);
+    /**
+     * Converts a string to camelCase.
+     *
+     * @param {string} str - The input string.
+     * @returns {string} - The formatted string.
+     */
+    const toCamelCase = (str) => {
+        // Use the toPascalCase function to format the string
+        const pascalCase = toPascalCase(str);
+        // Make the first character in a string lower case
+        return pascalCase.charAt(0).toLowerCase() + pascalCase.slice(1);
+    };
 
-    switch (caseSelector.value) {
-        case "snakeCase":
-            formattedString = toSnakeCase(formattedString);
-            break;
-        case "kebabCase":
-            formattedString = toKebabCase(formattedString);
-            break;
-        case "camelCase":
-            formattedString = toCamelCase(formattedString);
-            break;
-        case "pascalCase":
-            formattedString = toPascalCase(formattedString);
-            break;
-        default:
-            console.error("Unexpected case value");
-            break;
-    }
+    /**
+     * Formats the input string based on the selected case and copies it to the clipboard.
+     */
+    const formatString = () => {
+        const caseSelector = document.getElementById("case-select").value;
+        let formattedString = String(stringToFormatInput.value);
 
-    stringToFormat.value = formattedString;
+        if (!formattedString) {
+            console.warn("Input string is empty");
+            return;
+        }
 
-    // copy formatted string to clipboard
-    stringToFormat.select();
-    navigator.clipboard
-        .writeText(formattedString)
-        .then(() => console.log("Text successfully copied to clipboard"))
-        .catch((err) => console.error("Unable to copy text to clipboard", err));
-};
+        switch (caseSelector) {
+            case "snakeCase":
+                formattedString = toSnakeCase(formattedString);
+                break;
+            case "kebabCase":
+                formattedString = toKebabCase(formattedString);
+                break;
+            case "camelCase":
+                formattedString = toCamelCase(formattedString);
+                break;
+            case "pascalCase":
+                formattedString = toPascalCase(formattedString);
+                break;
+            default:
+                console.error(`Unexpected case value: ${caseSelector}`);
+                break;
+        }
 
-searchButton.addEventListener("click", formatString);
+        stringToFormatInput.value = formattedString;
 
-/**
- * Adds a keypress event listener for the Enter key on the string input element.
- *
- * @param {KeyboardEvent} e - The key event.
- */
-stringToFormat.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-        formatString();
-    }
-});
+        // copy formatted string to clipboard
+        stringToFormatInput.select();
+        navigator.clipboard
+            .writeText(formattedString)
+            .then(() => console.log("Text successfully copied to clipboard"))
+            .catch((err) =>
+                console.error("Unable to copy text to clipboard", err)
+            );
+    };
 
-window.addEventListener("keyup", handleSecretCodeCheck);
+    /**
+     * Sets focus on the string-to-format input field when the page loads.
+     */
+    const setFocusOnLoad = () => {
+        stringToFormatInput.focus();
+    };
+
+    searchButton.addEventListener("click", formatString);
+
+    /**
+     * Adds a keypress event listener for the Enter key on the string input element.
+     *
+     * @param {KeyboardEvent} e - The key event.
+     */
+    stringToFormatInput.addEventListener("keyup", (e) => {
+        if (e.key === "Enter") {
+            formatString();
+        }
+    });
+
+    // Call to check if the string input matches the secret code
+    window.addEventListener("keyup", handleSecretCodeCheck);
+
+    // Call the setFocusOnLoad function when the page has finished loading
+    window.addEventListener("load", setFocusOnLoad);
+})();
